b082eb4cf1db7d8309c8dd76d9c3abf1667da249
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-05-29 00:26:21 +0300

    add close modal button feature

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 // и просмотра полноразмерного изображения в модальном окне.
 
 // Разбей задание на несколько подзадач:
-// Создание и рендер разметки по массиву данных и предоставленному шаблону.
-// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-// Открытие модального окна по клику на элементе галереи.
-// Подмена значения атрибута src элемента img.lightbox__image.
+// Создание и рендер разметки по массиву данных и предоставленному шаблону. +
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения. +
+// Открытие модального окна по клику на элементе галереи. +
+// Подмена значения атрибута src элемента img.lightbox__image. +
 // Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
 // Очистка значения атрибута src элемента img.lightbox__image.
 // Это необходимо для того, чтобы при следующем открытии модального окна,
@@ -15,17 +15,22 @@
 import images from './gallery-items.js';
 
 // elements parsing
-const galleryRef = document.querySelector('.js-gallery');
+const galleryRef = document.querySelector('ul.js-gallery');
 const galleryModalWindowRef = document.querySelector('.js-lightbox');
-const galleryModalWindowImageRef = document.querySelector('.lightbox__image');
+const galleryModalWindowImageRef = document.querySelector(
+  'img.lightbox__image',
+);
+const modalWindowCloseBtnRef = document.querySelector(
+  'button[data-action="close-lightbox"]',
+);
 
 // markup creating
 const makeGalleryMarkup = ({ preview, original, description }) => {
   // href="${original}"
   return `
-<li class="gallery__item">
+  <li class="gallery__item">
   <a
-    class="gallery__link"
+      class="gallery__link"
   >
     <img
       class="gallery__image"
@@ -41,17 +46,21 @@ const makeGalleryMarkup = ({ preview, original, description }) => {
 const makeGallery = images.map(makeGalleryMarkup).join('');
 galleryRef.insertAdjacentHTML('beforeend', makeGallery);
 
-// listener adding
+// listeners adding
 galleryRef.addEventListener('click', onGalleryImageClick);
+modalWindowCloseBtnRef.addEventListener('click', onModalWindowCloseBtnClick);
 
+// callback functions:
 function onGalleryImageClick(event) {
   const originalImageUrl = event.target.dataset.source;
-  const imageDescription = event.target.getAttribute('alt');
+  const imageDescription = event.target.alt;
 
   galleryModalWindowImageRef.setAttribute('src', originalImageUrl);
   galleryModalWindowImageRef.setAttribute('alt', imageDescription);
 
   galleryModalWindowRef.classList.add('is-open');
+}
 
-  // console.log(event.target.attributes);
+function onModalWindowCloseBtnClick() {
+  galleryModalWindowRef.classList.remove('is-open');
 }
