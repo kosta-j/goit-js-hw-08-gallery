2047f4b143a0c3ab743283bd585f6c1dfe3caa72
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-05-29 01:45:42 +0300

    add gallery key features

--- script.js
+++ script.js
@@ -7,15 +7,15 @@
 // Реализация делегирования на галерее ul.js-gallery и получение url большого изображения. +
 // Открытие модального окна по клику на элементе галереи. +
 // Подмена значения атрибута src элемента img.lightbox__image. +
-// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
-// Очистка значения атрибута src элемента img.lightbox__image.
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"]. +
+// Очистка значения атрибута src элемента img.lightbox__image.+
 // Это необходимо для того, чтобы при следующем открытии модального окна,
 // пока грузится изображение, мы не видели предыдущее.
 'use strict';
 import images from './gallery-items.js';
 
 // elements parsing
-const galleryRef = document.querySelector('ul.js-gallery');
+const galleryRef = document.querySelector('.js-gallery');
 const galleryModalWindowRef = document.querySelector('.js-lightbox');
 const galleryModalWindowImageRef = document.querySelector(
   'img.lightbox__image',
@@ -23,6 +23,7 @@ const galleryModalWindowImageRef = document.querySelector(
 const modalWindowCloseBtnRef = document.querySelector(
   'button[data-action="close-lightbox"]',
 );
+const modalOverlayRef = document.querySelector('.lightbox__overlay');
 
 // markup creating
 const makeGalleryMarkup = ({ preview, original, description }) => {
@@ -46,21 +47,61 @@ const makeGalleryMarkup = ({ preview, original, description }) => {
 const makeGallery = images.map(makeGalleryMarkup).join('');
 galleryRef.insertAdjacentHTML('beforeend', makeGallery);
 
+// array of image original URLs
+const arrayImages = images.map(image => image.original);
+
 // listeners adding
 galleryRef.addEventListener('click', onGalleryImageClick);
 modalWindowCloseBtnRef.addEventListener('click', onModalWindowCloseBtnClick);
+modalOverlayRef.addEventListener('click', onModalWindowCloseBtnClick);
+document.addEventListener('keydown', onKeydown);
 
 // callback functions:
+
 function onGalleryImageClick(event) {
   const originalImageUrl = event.target.dataset.source;
   const imageDescription = event.target.alt;
 
-  galleryModalWindowImageRef.setAttribute('src', originalImageUrl);
-  galleryModalWindowImageRef.setAttribute('alt', imageDescription);
+  galleryModalWindowImageRef.src = originalImageUrl;
+  galleryModalWindowImageRef.alt = imageDescription;
 
   galleryModalWindowRef.classList.add('is-open');
 }
 
+// common function for all modal close events
 function onModalWindowCloseBtnClick() {
   galleryModalWindowRef.classList.remove('is-open');
+  galleryModalWindowImageRef.src = '';
+}
+
+function onKeydown(event) {
+  if (!galleryModalWindowRef.className.includes('is-open')) {
+    return;
+  }
+
+  // Escape key
+  if (event.key === 'Escape') {
+    onModalWindowCloseBtnClick();
+    return;
+  }
+
+  // Arrow keys
+  let newID;
+  const currentID = arrayImages.indexOf(galleryModalWindowImageRef.src);
+
+  if (event.key === 'ArrowLeft') {
+    newID = currentID - 1;
+    if (newID === -1) {
+      newID = arrayImages.length - 1;
+    }
+  }
+
+  if (event.key === 'ArrowRight') {
+    newID = currentID + 1;
+    if (newID === arrayImages.length) {
+      newID = 0;
+    }
+  }
+
+  galleryModalWindowImageRef.src = arrayImages[newID];
 }
